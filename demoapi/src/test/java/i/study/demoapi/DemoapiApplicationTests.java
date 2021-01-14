581c55bbf01faf0f3fa50bc5d1ec62e58e571041
mwkang <augustman8808@gmail.com>
2021-01-14 12:43:27 +0900

    init project

---
+++ demoapi/src/test/java/i/study/demoapi/DemoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package i.study.demoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
